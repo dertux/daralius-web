ca62706eb8df0b3308c82cba4b1140b886eb5706
dertux <seba.hurtado@duocuc.cl>
2024-11-23 22:01:12 -0300

    Enlace de descarga APK

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,28 @@ export default function DaraliusLanding() {
     Ver repositorio en GitHub
   </a>
 </div>
+<a 
+        href="https://drive.google.com/file/d/1PuXILyAEIc0OMTBG7p7z_u8nLjJqi274/view?usp=sharing" 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center justify-center gap-2 px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition-colors w-full mt-8"
+      >
+        <svg 
+          className="w-6 h-6" 
+          fill="currentColor" 
+          viewBox="0 0 87.3 78" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6.6 66.85l3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z" fill="#0066da"/>
+          <path d="M43.65 25l-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0-1.2 4.5h27.5z" fill="#00ac47"/>
+          <path d="M73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z" fill="#ea4335"/>
+          <path d="M43.65 25l13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z" fill="#00832d"/>
+          <path d="M59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z" fill="#2684fc"/>
+          <path d="M73.4 26.5l-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z" fill="#ffba00"/>
+        </svg>
+        Descarga la beta
+      </a>
+
         </div>
         <div className="lg:w-1/2">
         <div className="relative">
